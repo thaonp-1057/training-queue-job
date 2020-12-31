347ef595298adea559fecf340e94a1bab19ec033
nguyen.phuong.thao-c <SUN-ASTERISK\nguyen.phuong.thao-c@i121057-pc.sun-asterisk.com>
2020-12-31 10:18:32 +0700

    init project

---
+++ app/Models/ScheduleUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScheduleUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'schedule_user';
+
+    protected $fillable = [
+        'sending_time',
+        'status',
+        'user_id',
+        'sender',
+        'schedule_id',
+    ];
+}
---
+++ resources/lang/vi/app.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'layout' => [
+		'new_schedule' => 'New Schedule',
+		'schedules' => 'Schedules',
+	],
+	'new_schedule' => [
+		'title' => 'New Schedule',
+		'submit' => 'Submit',
+		'content' => 'Content',
+		'schedule_time' => 'Schedule Time',
+    ],
+    'message' => [
+        'schedule' => [
+            'success' => 'The schedule is successfully created',
+            'failed' => 'The Schedule is created unsuccessfully',
+        ],
+    ],
+];
