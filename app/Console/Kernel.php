29e86b6009b7ee9fe72d473bb67dc555fb8735e5
thaonp-1057 <nguyen.phuong.thao-c@sun-asterisk.com>
2020-12-31 18:07:53 +0700

    handel schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,8 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-        $schedule->command('email:report')->everyMinute();
+        $schedule->command('schedules:handle')->everyMinute();
     }
 
     /**
